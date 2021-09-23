ea5290488c5429de9df64a608014e0046ddf81d0
yzheng51 <y.zheng211@gmail.com>
2021-09-24 01:00:08 +0800

    Use KFold, Specify feats, Update params

--- core/train_gbm.py
+++ core/train_gbm.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
-from sklearn.model_selection import GroupKFold
+from sklearn.model_selection import KFold
 from joblib import Parallel, delayed
 
 from feats import get_book_feat, get_trade_feat, get_time_stock_feat
@@ -60,33 +60,137 @@ timer.stop()
 print("Traing model...")
 timer.start()
 
-feats = train.columns.drop(["row_id", "stock_id", "time_id", "target"]).tolist()
+feats = [
+    "book_time_id_count",
+    "book_wap1_mean",
+    "book_wap1_std",
+    "book_wap1_max_sub_min",
+    "book_wap2_mean",
+    "book_wap2_std",
+    "book_wap2_max_sub_min",
+    "book_log_return_wap1_mean",
+    "book_log_return_wap1_std",
+    "book_log_return_wap1_abs_sum",
+    "book_log_return_wap1_rv",
+    "book_log_return_wap2_mean",
+    "book_log_return_wap2_std",
+    "book_log_return_wap2_abs_sum",
+    "book_log_return_wap2_rv",
+    "book_wap_balance_mean",
+    "book_wap_balance_std",
+    "book_wap_balance_abs_sum",
+    "book_price_spread1_sum",
+    "book_price_spread1_mean",
+    "book_price_spread1_std",
+    "book_price_spread2_sum",
+    "book_price_spread2_mean",
+    "book_price_spread2_std",
+    "book_bid_spread_sum",
+    "book_bid_spread_mean",
+    "book_bid_spread_std",
+    "book_ask_spread_sum",
+    "book_ask_spread_mean",
+    "book_ask_spread_std",
+    "book_bid_ask_spread_mean",
+    "book_bid_ask_spread_std",
+    "book_bid_ask_spread_abs_sum",
+    "book_total_volume_sum",
+    "book_total_volume_mean",
+    "book_total_volume_std",
+    "book_volume_imbalance_mean",
+    "book_volume_imbalance_std",
+    "book_volume_imbalance_abs_sum",
+    "book_log_return_wap1_rv_diff",
+    "book_log_return_wap1_rv_pct_change",
+    "book_log_return_wap2_rv_diff",
+    "book_log_return_wap2_rv_pct_change",
+    "book_last_300_log_return_wap1_rv",
+    "book_last_300_log_return_wap2_rv",
+    "trade_time_id_count",
+    "trade_price_mean",
+    "trade_price_std",
+    "trade_price_max_sub_min",
+    "trade_log_return_mean",
+    "trade_log_return_std",
+    "trade_log_return_abs_sum",
+    "trade_log_return_rv",
+    "trade_size_sum",
+    "trade_size_max_sub_min",
+    "trade_order_count_sum",
+    "trade_order_count_max_sub_min",
+    "trade_amount_sum",
+    "trade_amount_mean",
+    "trade_amount_std",
+    "trade_amount_max_sub_min",
+    "trade_size_div_order_count_mean",
+    "trade_size_div_order_count_std",
+    "trade_seconds_diff_mean",
+    "trade_seconds_diff_std",
+    "trade_seconds_diff_abs_sum",
+    "trade_seconds_diff_rv",
+    "trade_log_return_rv_diff",
+    "trade_log_return_rv_pct_change",
+    "trade_last_300_log_return_rv",
+    "trade_last_300_seconds_diff_rv",
+    "time_id_book_log_return_wap1_rv_mean",
+    "time_id_book_log_return_wap1_rv_std",
+    "time_id_book_log_return_wap2_rv_mean",
+    "time_id_book_log_return_wap2_rv_std",
+    "time_id_trade_log_return_rv_mean",
+    "time_id_trade_log_return_rv_std",
+    "time_id_trade_seconds_diff_rv_mean",
+    "time_id_trade_seconds_diff_rv_std",
+    "time_id_book_last_300_log_return_wap1_rv_mean",
+    "time_id_book_last_300_log_return_wap1_rv_std",
+    "time_id_book_last_300_log_return_wap2_rv_mean",
+    "time_id_book_last_300_log_return_wap2_rv_std",
+    "time_id_trade_last_300_log_return_rv_mean",
+    "time_id_trade_last_300_log_return_rv_std",
+    "time_id_trade_last_300_seconds_diff_rv_mean",
+    "time_id_trade_last_300_seconds_diff_rv_std",
+    "stock_id_book_log_return_wap1_rv_mean",
+    "stock_id_book_log_return_wap1_rv_std",
+    "stock_id_book_log_return_wap2_rv_mean",
+    "stock_id_book_log_return_wap2_rv_std",
+    "stock_id_trade_log_return_rv_mean",
+    "stock_id_trade_log_return_rv_std",
+    "stock_id_trade_seconds_diff_rv_mean",
+    "stock_id_trade_seconds_diff_rv_std",
+    "stock_id_book_last_300_log_return_wap1_rv_mean",
+    "stock_id_book_last_300_log_return_wap1_rv_std",
+    "stock_id_book_last_300_log_return_wap2_rv_mean",
+    "stock_id_book_last_300_log_return_wap2_rv_std",
+    "stock_id_trade_last_300_log_return_rv_mean",
+    "stock_id_trade_last_300_log_return_rv_std",
+    "stock_id_trade_last_300_seconds_diff_rv_mean",
+    "stock_id_trade_last_300_seconds_diff_rv_std",
+]
 x_train = train[feats]
 y_train = train["target"].values
 
-kfold = GroupKFold(n_splits=n_splits)
+kfold = KFold(n_splits=n_splits, shuffle=True, random_state=1024)
 
 stack_train = np.zeros(x_train.shape[0])
 feats_impt = np.zeros(len(feats))
 rmspe_mean = 0
 
 
-for i, (train_idx, valid_idx) in enumerate(kfold.split(x_train, y_train, train["time_id"])):
+for i, (train_idx, valid_idx) in enumerate(kfold.split(x_train)):
     print(f'Stacking: {i + 1}/{n_splits}... ')
     model = lgb.LGBMRegressor(
         boosting="gbdt",
         importance_type='gain',
         learning_rate=0.01,
-        max_depth=8,
-        num_leaves=60,
+        max_depth=7,
+        num_leaves=80,
         min_child_weight=20,
-        n_estimators=2000,
+        n_estimators=3000,
         lambda_l2=1,
         bagging_freq=1,
-        bagging_fraction=0.7,
-        feature_fraction=0.5,
-        random_state=54786,
-        bagging_seed=12783,
+        bagging_fraction=0.6,
+        feature_fraction=0.3,
+        random_state=55384,
+        bagging_seed=13881,
         n_jobs=50,
     )
     model.fit(
