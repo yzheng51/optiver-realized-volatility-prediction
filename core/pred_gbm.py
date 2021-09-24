3fb0c14a150d699c61971a8a83b2379efe69e6d0
yzheng51 <y.zheng211@gmail.com>
2021-09-24 21:50:23 +0800

    Update code

--- core/pred_gbm.py
+++ core/pred_gbm.py
@@ -37,22 +37,22 @@ print(f"Number of features: {len(GBM_FEATS)}")
 x_test = test[GBM_FEATS]
 stack_test = np.zeros(x_test.shape[0])
 
-for i in range(n_splits):
+for idx in range(n_splits):
     start = time.perf_counter()
-    model_path = f"../input/orvplgb/{i + 1}.txt"
+    model_path = f"../input/orvplgb/{idx + 1}.txt"
     model = lgb.Booster(model_file=model_path)
     y_pred = model.predict(x_test)
 
     stack_test += y_pred
     duration = time.perf_counter() - start
     print(
-        f"Fold: {i + 1}/{n_splits}... ",
-        f"Model path: {model_path}...",
+        f"Fold: {idx + 1}/{n_splits}... ",
+        f"Model path: `{model_path}`...",
         f"Elapse time: {duration:.2f}s...",
     )
 
 stack_test /= n_splits
 
 test["target"] = stack_test
-test[["row_id", "target"]].to_csv("submission.csv",index = False)
+test[["row_id", "target"]].to_csv("submission.csv", index=False)
 test[["row_id", "target"]]
